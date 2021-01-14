d5945c744784073fec2d1051f54020dcabc1e0ca
StanDarJeep <stzhao2@gmail.com>
2021-01-13 19:37:21 -0800

    added art

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -18,10 +18,12 @@ public class Table {
 
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
+    private final Board chessBoard;
 
     private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
     private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
     private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+    private static String defaultImagesPath = "art/pieces/plain/";
     private final Color lightTileColor = Color.decode("#FFFACD");
     private final Color darkTileColor = Color.decode("#593E1A");
 
@@ -31,6 +33,7 @@ public class Table {
         final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+        this.chessBoard = Board.createStandardBoard();
         this.boardPanel = new BoardPanel();
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
 
@@ -92,15 +95,15 @@ public class Table {
             this.tileId = tileId;
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
+            assignTilePieceIcon(chessBoard);
             validate();
         }
 
         private void assignTilePieceIcon(final Board board) {
             this.removeAll();
             if (board.getTile(this.tileId).isTileOccupied()) {
-                String pieceIconPath = "";
                 try {
-                    final BufferedImage image = ImageIO.read(new File(pieceIconPath +
+                    final BufferedImage image = ImageIO.read(new File(defaultImagesPath +
                         board.getTile(this.tileId).getPiece().getPieceAlliance().toString().
                         substring(0, 1) + board.getTile(this.tileId).getPiece().toString() +
                         ".gif"));
